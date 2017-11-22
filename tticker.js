7fd59cad87c3c27565887816fdec3b58c15e4250
Glenn Kessler <info@glenn-kessler.de>
2017-11-22 16:08:28 +0100

    tticker.js now initializes an array of arrays holding counter values

--- tticker.js
+++ tticker.js
@@ -1,13 +1,37 @@
 window.onload = function () {
-  divdebug= document.getElementById('debug')
-  _p = divdebug.getElementsByTagName('p')
+  tticker.init()
+
+  _debug = document.getElementById('debug')
+  _p = _debug.getElementsByTagName('p')
 
   for (let i = 0; i < _p.length; i++) {
     _p[i].onclick = function() { 
       this.innerHTML =  'YOU CLICKED ME!'
     }
     _p[i].oncontextmenu = function() {
-      this.innerHTML = 'YOU RIGHT CLICKED ME!'
+      this.innerHTML = 'YOU CLICKED ME RIGHT!'
+      return false
     }
   }
+}
+
+var tticker = {
+  sheet : new Array(6),
+  cols : 7,
+  rows : 6,
+  frei : '0',
+  
+  init : function()
+  {
+    let cols = this.cols;
+    let rows = this.rows;
+    let frei = this.frei;
+
+    for (let y = 0; y < rows ; y++) { 
+      this.sheet[y] = new Array(cols);
+  
+      for (let x = 0; x < cols; x++)
+        this.sheet[y][x] = frei;
+    }
+  },
 }
