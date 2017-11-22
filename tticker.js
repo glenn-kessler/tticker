42b33287652d16ec578175e23a5e51963ebee9e4
Glenn Kessler <info@glenn-kessler.de>
2017-11-22 13:55:29 +0100

    add event on rightclick

--- tticker.js
+++ tticker.js
@@ -1,10 +1,13 @@
 window.onload = function () {
   divdebug= document.getElementById('debug')
-  p = divdebug.getElementsByTagName('p')
-  for (let i = 0; i < p.length; i++) {
-    p[i].onclick = function()
-    { 
+  _p = divdebug.getElementsByTagName('p')
+
+  for (let i = 0; i < _p.length; i++) {
+    _p[i].onclick = function() { 
       this.innerHTML =  'YOU CLICKED ME!'
+    }
+    _p[i].oncontextmenu = function() {
+      this.innerHTML = 'YOU RIGHT CLICKED ME!'
     }
   }
 }
